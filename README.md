1f850c2366a8fddbca23b85559e4541fa7abf773
s1ghhh <sghinscu@163.com>
2024-04-30 14:24:16 +0000

    upload cma-es

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# Merging_CMA_ES
+## Environment
+1. **lm-evaluation-harness**
+
+   Install a new version of lm_eval
+
+   ```shell
+   git clone https://github.com/s1ghhh/lm-evaluation-harness.git
+   cd lm-evaluation-harness
+   git checkout offset_by_id
+   
+   git log
+   #commit cc499b52c265853fe92305af1897eb137f3036e3 (HEAD -> offset_by_id, #origin/offset_by_id)
+    #Author: s1ghhh <sghinscu@163.com>
+    #Date:   Sun Apr 21 02:31:59 2024 +0000
+   
+   ```
+   
+   Ensure the version of the following Python libraries
+   
+   ```shell
+   optuna=3.6.1
+   accelerate=0.27.2
+   transformers=4.36.2
+   lm_eval=0.4.0
+   torch=2.1.2
+   ```
+   
+   Then run the following scripts to check if the result is `0.7372`. If the result is `0.7372`, then the version is correct; otherwise, please refer to `./requirements_lm_eval.txt`
+
+   ```shell
+    CUDA_VISIBLE_DEVICES=0 accelerate launch -m lm_eval --model hf \
+     --model_args pretrained=allenai/tulu-2-dpo-7b,trust_remote_code=True,dtype="bfloat16" \
+     --tasks winogrande \
+     --num_fewshot 5 \
+     --batch_size 1 
+   
+    CUDA_VISIBLE_DEVICES=0,1 accelerate launch -m lm_eval --model hf \
+     --model_args pretrained=allenai/tulu-2-dpo-7b,trust_remote_code=True,dtype="bfloat16" \
+     --tasks winogrande \
+     --num_fewshot 5 \
+     --batch_size 1 
+   ```
+
+   Now please add the dataset of tinybenchmarks to the lm_eval
+
+   ```shell
+      cp -r <path of this repo, not the lm_eval>/lm_eval/tasks/tinyMMLU <path of lm_eval>/lm_eval/tasks/
+      cp <path of this repo, not the lm_eval>/lm_eval/tasks/arc/arc_easy.yaml <path of lm_eval>/lm_eval/tasks/arc/
+      cp <path of this repo, not the lm_eval>/lm_eval/tasks/gsm8k/gsm8k.yaml <path of lm_eval>/lm_eval/tasks/gsm8k/
+      cp <path of this repo, not the lm_eval>/lm_eval/tasks/winogrande/default.yaml <path of lm_eval>/lm_eval/tasks/winogrande/
+   ```
+   For tasks such as `mmlu_tiny, gsm8k, arc_challenge, winogrande` in the lm-eval, we will use the dataset of tinybenchmarks.
+
+
+2. **bigcode-evaluation-harness**
+
+   Install bigcode-evaluation-harness
+
+   ```shell
+      git clone https://github.com/bigcode-project/bigcode-evaluation-harness.git
+      cd bigcode-evaluation-harness
+      git checkout 00967d12093ef614de7bdad0772aed8e4118f1fd
+   ```
+   
+   Now please replace the `mbpp.py` to use a smaller validation set for evaluation
+   
+   ```shell
+      cp <path of this repo, not the bigcode>/bigcode_eval/tasks/mbpp.py <path of bigcode>/bigcode_eval/tasks/
+   ```
+
+## Run and eval
+
+1. **Running evolutionary strategies**
+
+   Currently, only single cards are supported, and disk reads are frequent. The code is not efficient and will be improved in the future.
+
+   For scripts `ties_which8.py`, `ties_which4.py`, `linear_which8.py`, `linear_which4.py`, the following parameters need to be adjusted.
+   ```shell
+      # The root directory for executing this script, and each .py script should have a separate root directory
+      root_path='/workspace/0423_tinybenchmarks/SEARCH/evo/0428_new_zoo/which8/tinybench_linear'
+      # Which GPU is the script running on
+      GPU_idx='5'
+      # the path of bigcode-evaluation-harness
+      path_of_bigcode='/workspace/0423_tinybenchmarks/bigcode-evaluation-harness'
+   ```
+   
+   Then please run the script and collect the results of each iteration.
+
+   ```shell
+      nohup python -u ties_which8.py >> ties_which8.out &
+   ```
+   
+   The results of each round will be recorded in `ties_which8.out`
+
+2. **Running evolutionary strategies**
+
+   After obtaining the best combination, please evaluate it using normal lm-eval (w.o tinybenchmarks) and bigcode (use testset of mbpp instead of valset).
---
+++ linear_which8.py
@@ -0,0 +1,112 @@
+import optuna
+import subprocess
+import os
+import json
+env = os.environ.copy()
+env['MKL_THREADING_LAYER'] = 'GNU'
+env['MKL_SERVICE_FORCE_INTEL'] = '1'
+
+root_path='/workspace/0423_tinybenchmarks/SEARCH/evo/0428_new_zoo/which8/tinybench_linear'
+GPU_idx='5'
+path_of_bigcode='/workspace/0423_tinybenchmarks/bigcode-evaluation-harness'
+
+def objective(trial):
+    weights_list = [
+        trial.suggest_float(f"weight_{i}", 0.0, 1.0)
+        for i in range(8)
+    ]
+    try:
+        shell_script_content = f"""#!/bin/bash
+root_path={root_path}
+models=("lmsys/vicuna-7b-v1.5" "meta-llama/Llama-2-7b-chat-hf" "teknium/OpenHermes-7B" "garage-bAInd/Platypus2-7B" "neuralmagic/Llama-2-7b-evolcodealpaca" "meta-math/MetaMath-7B-V1.0" "migtissera/Synthia-7B-v1.2" "PygmalionAI/pygmalion-2-7b")
+
+weights=("{weights_list[0]}" "{weights_list[1]}" "{weights_list[2]}" "{weights_list[3]}" "{weights_list[4]}" "{weights_list[5]}" "{weights_list[6]}" "{weights_list[7]}")
+
+
+echo -e "models:\\n" > tmpyml.yml
+w_count=0
+for model in "${{models[@]}}"; do
+    echo -e "  - model: $model\\n    parameters:\\n      weight: ${{weights[$((w_count))]}}\\n" >> tmpyml.yml
+    ((w_count++))
+done
+    echo -e "merge_method: linear\\ndtype: bfloat16" >> tmpyml.yml
+
+# unset models[${{#models[@]}}-1]
+CUDA_VISIBLE_DEVICES={GPU_idx} mergekit-yaml tmpyml.yml ./tmp_model --cuda --random-seed 42
+# cp ./config.json ./tmp_model
+
+result_float=0
+tasks=("mmlu_tiny" "gsm8k" "arc_challenge" "winogrande")
+key1=("mmlu_tiny" "gsm8k" "arc_challenge" "winogrande")
+key2=("acc,none" "exact_match,get-answer" "acc_norm,none" "acc,none")
+k=0
+for task in "${{tasks[@]}}"; do
+    CUDA_VISIBLE_DEVICES={GPU_idx} lm_eval --model hf \
+    --model_args pretrained=./tmp_model,trust_remote_code=True,dtype="bfloat16" \
+    --tasks $task \
+    --batch_size 1 \
+    --output_path ./${{task}}_tmp.json
+    result=$(jq -r ".results.${{key1[$k]}}.\\"${{key2[$k]}}\\"" ./${{task}}_tmp.json)
+    tmp_result=$(echo "$result" | bc)
+    result_float=$(echo "$result_float + $tmp_result" | bc)
+    rm ./${{task}}_tmp.json
+    ((k++))
+done
+
+
+
+export CUDA_VISIBLE_DEVICES={GPU_idx}
+python {path_of_bigcode}/main.py \
+    --model $root_path/tmp_model \
+    --tasks mbpp \
+    --max_length_generation 1024 \
+    --do_sample True \
+    --n_samples 1 \
+    --top_p 0.95 \
+    --batch_size 1 \
+    --temperature 0.2 \
+    --precision bf16 \
+    --trust_remote_code \
+    --allow_code_execution \
+    --use_auth_token \
+    --metric_output_path $root_path/mbpp_tmp.json
+
+
+# cd $root_path
+result=$(jq -r '.mbpp."pass@1"' ./mbpp_tmp.json)
+tmp_result=$(echo "$result" | bc)
+result_float=$(echo "$result_float + $tmp_result" | bc)
+rm ./mbpp_tmp.json
+
+
+echo $result_float
+echo -e $result_float > ./tmp_results.txt
+
+"""
+        script_filename = 'tmp_script.sh'
+        with open(script_filename, 'w') as script_file:
+            script_file.write(shell_script_content)
+
+        os.chmod(script_filename, 0o755)
+
+        result = subprocess.run(['./tmp_script.sh'], capture_output=True, text=True, shell=True, env=env)
+
+        if result.returncode == 0:
+            with open('./tmp_results.txt', 'r') as f:
+                acc = float(f.readline())
+
+            print('------------------------------')
+            print('weights_list: ', weights_list)
+            print('acc: ', acc)
+            os.remove('./tmp_results.txt')
+            return -float(acc)
+        else:
+            print("STDOUT:", result.stdout)
+            print("STDERR:", result.stderr)
+            print("Return Code:", result.returncode)
+            return 0.0
+    except:
+        return 0.0
+sampler = optuna.samplers.CmaEsSampler(popsize=10, sigma0=1/6, seed=42)
+study = optuna.create_study(sampler=sampler)
+study.optimize(objective, n_trials=500, show_progress_bar=True)
